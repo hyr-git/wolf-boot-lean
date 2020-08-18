2f2b458b7ab417036bdee7fa3bc016aeb162712a
maliujiang <maliujiang@carsir.xin>
2020-08-18 14:27:15 +0800

    添加单独使用的rabbitmq

--- wolf-boot-lean-rabbitmq/src/main/java/com/hyr/lean/rabbitmq/RabbitMqApp.java
+++ wolf-boot-lean-rabbitmq/src/main/java/com/hyr/lean/rabbitmq/RabbitMqApp.java
@@ -13,7 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *     而当发送消息的时候,只有指定的key和该模式相配的消息，消息才会被发送到该消息队列中。
  *     fanout是路由广播的形式,将会把消息发给绑定他的全部队列，即便设置了key也会被忽略。
  * @author m1832
- *
+ * 
+ * AMQP的主要特征是面向消息、队列、路由(包括点对点和发布订阅)、可靠性、安全
+ * 用于在分布式系统中存储转发消息,在易用性、可扩展、高可用等。
  */
 @SpringBootApplication
 public class RabbitMqApp 
