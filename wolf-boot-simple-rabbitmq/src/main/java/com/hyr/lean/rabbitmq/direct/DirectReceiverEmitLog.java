4a03b3f6e0d156a96560fb56582216ce7a8bae72
maliujiang <maliujiang@carsir.xin>
2020-09-29 15:26:06 +0800

    提交代码

--- wolf-boot-simple-rabbitmq/src/main/java/com/hyr/lean/rabbitmq/direct/DirectReceiverEmitLog.java
+++ wolf-boot-simple-rabbitmq/src/main/java/com/hyr/lean/rabbitmq/direct/DirectReceiverEmitLog.java
@@ -12,9 +12,20 @@ import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
+
+/*****
+ * Direct模式的绑定
+new Binding(someQueue, someDirectExchange, "foo.bar"）
+Fanout模式的绑定
+new Binding(someQueue, someFanoutExchange)
+Topic模式的绑定
+new Binding(someQueue, someTopicExchange, "foo.*")
+ * @author mlj
+ *
+ */
 public class DirectReceiverEmitLog {
 
-    private static final String EXCHANGE_NAME = "direct_logs";
+	private static final String EXCHANGE_NAME = "direct_logs";
 
 	public static void main(String[] args) throws IOException, TimeoutException {
 		ConnectionFactory factory = null;
@@ -22,46 +33,42 @@ public class DirectReceiverEmitLog {
 		Channel channel = null;
 		String queueName = "";
 		try {
-			
+
 			factory = new ConnectionFactory();
 			factory.setHost("127.0.0.1");
 			connection = factory.newConnection();
 			channel = connection.createChannel();
-			
-			//声明使用的exchange类型为DIRECT
+
+			// 声明使用的exchange类型为DIRECT
 			channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
 
-            queueName = channel.queueDeclare().getQueue();
+			queueName = channel.queueDeclare().getQueue();
 
-            /*
-             * 这里两个参数info和error表示绑定了两个routing key.
-             * 即如果发送routing key为info的消息该队列能接受到,若发送routing key为error，该队列也能收到
-             */
-            
-			String argv[] = {"info","error"};
-			 if (argv.length < 1){
-		            System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
-		            System.exit(1);
-		        }
-		     for(String severity : argv){
-		            channel.queueBind(queueName, EXCHANGE_NAME, severity);
-		        }
-		        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-		       
-		    Consumer consumer = new DefaultConsumer (channel){
-		    	 @Override
-		    	    public void handleDelivery(String consumerTag,
-		    	                               Envelope envelope,
-		    	                               AMQP.BasicProperties properties,
-		    	                               byte[] body)
-		    	        throws IOException
-		    	    {
-		    		 String message = new String(body, "UTF-8");
-		                System.out.println(" [x] Received '" + envelope.getRoutingKey() + "':'" + message + "'");
-		    	    }
-		    };  
-		    
-	        channel.basicConsume(queueName, true, consumer);
+			/*
+			 * 这里两个参数info和error表示绑定了两个routing key. 即如果发送routing
+			 * key为info的消息该队列能接受到,若发送routing key为error，该队列也能收到
+			 */
+
+			String argv[] = { "info", "error" };
+			if (argv.length < 1) {
+				System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+				System.exit(1);
+			}
+			for (String severity : argv) {
+				channel.queueBind(queueName, EXCHANGE_NAME, severity);
+			}
+			System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+			Consumer consumer = new DefaultConsumer(channel) {
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+						byte[] body) throws IOException {
+					String message = new String(body, "UTF-8");
+					System.out.println(" [x] Received '" + envelope.getRoutingKey() + "':'" + message + "'");
+				}
+			};
+
+			channel.basicConsume(queueName, true, consumer);
 
 		} finally {
 			// TODO: handle finally clause
---
+++ wolf-boot-simple-rabbitmq/src/main/java/com/hyr/lean/rabbitmq/topic/f.java
@@ -0,0 +1,5 @@
+package com.hyr.lean.rabbitmq.topic;
+
+public class f {
+
+}
