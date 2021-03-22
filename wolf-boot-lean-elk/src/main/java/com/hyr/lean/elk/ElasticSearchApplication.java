4ee159312de610945f3aba449a84e6721158b6a0
maliujiang <maliujiang@carsir.xin>
2021-03-22 14:27:27 +0800

    tijiao daim

---
+++ wolf-boot-lean-elk/src/main/java/com/hyr/lean/elk/ElasticSearchApplication.java
@@ -0,0 +1,22 @@
+package com.hyr.lean.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b><code>ElasticSearchApplication</code></b>
+ * <p/>
+ * Description:
+ * <p/>
+ * <b>Creation Time:</b> 2019/6/29 14:49.
+ *
+ * @author HuWeihui
+ */
+@SpringBootApplication
+public class ElasticSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchApplication.class, args);
+    }
+
+}
